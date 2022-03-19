195d8d7dcfa57df43e3d9773cfc4f3c21c8cc79f
avocadosamsam <kimwhayoung1717@gmail.com>
2022-03-19 14:34:20 +0900

    push all

---
+++ rocket_simulator/main.py
@@ -0,0 +1,14 @@
+num = int(input())
+i=0
+while i<num:
+    input1 = int(input())
+    input2=int(input())
+    result=input1+input2
+    print(result)
+    i+=1
+
+# for i in range(0, num):
+#     input1=int(input())
+#     input2=int(input())
+#     result=input1+input2
+#     print(result)
